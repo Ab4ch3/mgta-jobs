1cbafd494f081ef05a1bd3ac0b9365c667442df9
miguel <miiguel.abache@gmail.com>
2023-12-24 00:20:36 -0400

    HeadLine

---
+++ src/components/HeadLine.vue
@@ -0,0 +1,61 @@
+<template>
+  <section>
+    <h1 class="font-bold tracking-tighter text-8xl mb-14">
+      <span :class="actionClasses">{{ action }}</span>
+      <br />
+      For everyone
+    </h1>
+    <h2 class="text-3xl font-light">Find your next job at Mgta Jobs.</h2>
+  </section>
+</template>
+
+<script setup>
+import { computed, onBeforeUnmount, ref } from 'vue';
+
+let action = ref('Build');
+let interval = null;
+
+/* Computed properties */
+const actionClasses = computed(() => {
+  return {
+    [action.value.toLocaleLowerCase()]: true
+  };
+});
+
+/* Methods */
+const changeTitle = () => {
+  interval = setInterval(() => {
+    let actions = ['Build', 'Create', 'Design', 'Code'];
+    let currentActionIndex = actions.indexOf(action.value);
+    let nextActionIndex = (currentActionIndex + 1) % 4;
+    let nextAction = actions[nextActionIndex];
+    action.value = nextAction;
+  }, 3000);
+};
+
+/*
+### Lifecycle Hooks
+*/
+//Se ejecutara ante de crear el componente (onCreated)
+changeTitle();
+
+//Luego que desmonte el componente es recomendable limpiar el interval o el timeout
+onBeforeUnmount(() => {
+  clearInterval(interval);
+});
+</script>
+
+<style scoped>
+.build {
+  color: #1a73ed;
+}
+.create {
+  color: #34a853;
+}
+.design {
+  color: #f9ab00;
+}
+.code {
+  color: #d93025;
+}
+</style>
--- src/components/HeroDashboard.vue
+++ src/components/HeroDashboard.vue
@@ -3,14 +3,19 @@
     <section class="flex flex-col h-screen pt-10 pb-20 bg-yellow-200">
       <div class="grid grid-cols-12">
         <div class="grid-start-1 col-span-1 border border-blue-700">1 column</div>
+        <div class="grid-start-7 col-span-5 border border-blue-700">
+          <HeadLine />
+        </div>
         <div class="grid-start-2 col-span-5 border border-blue-700">5 column</div>
-        <div class="grid-start-7 col-span-5 border border-blue-700">5 colums</div>
         <div class="grid-start-11 col-span-1 border border-blue-700">1 columns</div>
       </div>
     </section>
   </main>
 </template>
 
-<script setup></script>
+<script setup>
+import HeadLine from '@/components/HeadLine.vue';
+</script>
 
 <style scoped></style>
+``
