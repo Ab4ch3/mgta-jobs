853abec25725d8435c2591fea0b9d65ec7f3fbd0
miguel <miiguel.abache@gmail.com>
2023-12-25 20:17:59 -0400

    Form search

--- src/components/HeroDashboard.vue
+++ src/components/HeroDashboard.vue
@@ -1,13 +1,14 @@
 <template>
   <main>
-    <section class="flex flex-col h-screen pt-10 pb-20 bg-yellow-200">
+    <section class="flex flex-col h-screen pt-10 pb-20">
       <div class="grid grid-cols-12">
-        <div class="grid-start-1 col-span-1 border border-blue-700">1 column</div>
-        <div class="grid-start-7 col-span-5 border border-blue-700">
+        <div class="grid-start-1 col-span-1"></div>
+        <div class="grid-start-7 col-span-5">
           <HeadLine />
+          <JobSearch />
         </div>
-        <div class="grid-start-2 col-span-5 border border-blue-700">5 column</div>
-        <div class="grid-start-11 col-span-1 border border-blue-700">1 columns</div>
+        <div class="grid-start-2 col-span-5"></div>
+        <div class="grid-start-11 col-span-1"></div>
       </div>
     </section>
   </main>
@@ -15,6 +16,7 @@
 
 <script setup>
 import HeadLine from '@/components/HeadLine.vue';
+import JobSearch from '@/components/JobSearchForm.vue';
 </script>
 
 <style scoped></style>
